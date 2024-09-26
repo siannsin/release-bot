d05b45904d63e9a57aa57f4fb3e29b070c7ef437
JanisV <opennumismt@gmail.com>
2024-09-26 10:57:09 +0300

    Get repo command

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,6 +1,7 @@
 import os
 
-from telegram import ForceReply, Update
+import github
+from telegram import ForceReply, Update, LinkPreviewOptions
 from telegram.ext import (
     Application,
     CommandHandler,
@@ -10,6 +11,7 @@ from telegram.ext import (
 )
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from github import Github
 
 from models import ChatSettings
 
@@ -44,9 +46,23 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
     await update.message.reply_text("Help!")
 
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Echo the user message."""
-    await update.message.reply_text(update.message.text)
+async def message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Add GitHub repo"""
+    repo_name = update.message.text
+    g = Github()
+    try:
+        repo = g.get_repo(repo_name)
+    except github.GithubException as e:
+        await update.message.reply_text("Sorry, I can't find that repo.")
+        return
+
+    await update.message.reply_html(
+        f"Added GitHub repo: <a href='{repo.html_url}'>{repo.full_name}</a>",
+        link_preview_options=LinkPreviewOptions(url=repo.html_url,
+                                                # is_disabled=True,
+                                                prefer_small_media=True,
+                                                ),
+    )
 
 
 def run_telegram_bot() -> None:
@@ -59,7 +75,7 @@ def run_telegram_bot() -> None:
     application.add_handler(CommandHandler("help", help_command))
 
     # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, message))
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
