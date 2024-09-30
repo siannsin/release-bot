40a50b4b511039db766607a9c76e61be6247a75f
JanisV <opennumismt@gmail.com>
2024-09-30 17:43:11 +0300

    Set SQLite journal mode to WAL

--- database.py
+++ database.py
@@ -1,5 +1,20 @@
+import sqlite3
+
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy import Engine, event
+
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    if isinstance(dbapi_connection, sqlite3.Connection):
+        # Look at https://kerkour.com/sqlite-for-servers
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA journal_mode=WAL")
+        cursor.execute("PRAGMA synchronous=NORMAL")
+        cursor.execute("PRAGMA busy_timeout=5000")
+        cursor.execute("PRAGMA temp_store=memory")
+        cursor.close()
 
 
 def init_database(app):
