27de16ccb850cda9db0b3469e60fdf01dd84e77d
JanisV <opennumismt@gmail.com>
2024-09-30 11:26:21 +0300

    Fix Config object

--- config.py
+++ config.py
@@ -8,4 +8,4 @@ class Config:
     TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
     GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI', f'sqlite:///{basedir}/data/db.sqlite')
-    SQLALCHEMY_ECHO = os.environ.get('SQL_DEBUG', False)
+    SQLALCHEMY_ECHO = os.environ.get('SQL_DEBUG', '').lower() in ('true', '1', 't')
