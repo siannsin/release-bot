7e2eabe177c2d503a2bbdecd92a040f5a44eed78
JanisV <ignatov.vit@gmail.com>
2024-10-17 14:16:46 +0300

    Refactor routes

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 __version__ = "0.2.4"
 
 import asyncio
-from http import HTTPStatus
 
-from flask import Flask, Response, request
+from flask import Flask
 from flask_apscheduler import APScheduler
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -52,26 +51,9 @@ else:
 scheduler.start()
 
 
-@app.route('/')
-async def index():
-    bot_me = await telegram_bot.get_me()
-    return (f'<a href="https://t.me/{bot_me.username}">{bot_me.first_name}</a> - a telegram bot for GitHub releases.'
-            '<br><br>'
-            'Source code available at <a href="https://github.com/JanisV/release-bot">release-bot</a>')
-
-
-@app.post("/telegram")
-async def telegram() -> Response:
-    """Handle incoming Telegram updates by putting them into the `update_queue`"""
-    if app.config['SITE_URL']:
-        await telegram_bot.webhook(request.json)
-        return Response(status=HTTPStatus.OK)
-    else:
-        return Response(status=HTTPStatus.NOT_IMPLEMENTED)
-
-
 if __name__ == '__main__':
     app.run()
 
-import scheduler  # noqa: E402
 import models  # noqa: E402
+import routes  # noqa: E402
+import scheduler  # noqa: E402
---
+++ routes.py
@@ -0,0 +1,23 @@
+from http import HTTPStatus
+
+from flask import Response, request
+
+from app import telegram_bot, app
+
+
+@app.route('/')
+async def index():
+    bot_me = await telegram_bot.get_me()
+    return (f'<a href="https://t.me/{bot_me.username}">{bot_me.first_name}</a> - a telegram bot for GitHub releases.'
+            '<br><br>'
+            'Source code available at <a href="https://github.com/JanisV/release-bot">release-bot</a>')
+
+
+@app.post("/telegram")
+async def telegram() -> Response:
+    """Handle incoming Telegram updates by putting them into the `update_queue`"""
+    if app.config['SITE_URL']:
+        await telegram_bot.webhook(request.json)
+        return Response(status=HTTPStatus.OK)
+    else:
+        return Response(status=HTTPStatus.NOT_IMPLEMENTED)
